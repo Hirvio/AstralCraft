9dc9048c18ed9fcfb55597ad1393a9a2ecadda99
Hirvio <oscar.rehnstrom@hotmail.se>
2014-06-28 00:16:13 +0200

    Test

---
+++ src/main/java/com/hirvio/astralcraft/AstralCraft.java
@@ -0,0 +1,11 @@
+package com.hirvio.astralcraft;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid="AstralCraft", name="Astral Craft", version="1.7.2-0.1")
+public class AstralCraft {
+
+    @Mod.Instance("AstralCraft")
+    public static AstralCraft instance;
+
+}
